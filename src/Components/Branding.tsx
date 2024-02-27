013ca8d31a9324c8051e844e1614b6f5bc2d8a06
SagarOad <124421949+SagarOad@users.noreply.github.com>
2024-02-27 11:49:58 -0900

    new changes

--- src/Components/Branding.tsx
+++ src/Components/Branding.tsx
@@ -1,195 +1,77 @@
-import { BsFillRocketTakeoffFill } from 'react-icons/bs';
-import { IoIosPeople } from 'react-icons/io';
-import { GiReceiveMoney } from 'react-icons/gi';
-
-
-
 const Branding = () => {
   return (
-    <div className="bg-white rounded-t-[50px]">
-      <div className="w-full py-20 xl:px-24  mx-auto">
-        <div className="xl:flex flex-col justify-between items-center md:flex-row">
-          <div className='basis-4/4 mb-12 xl:mb-0 px-12 md:basis-2/4'>
-          <div className="opacity-100 my-4 p-8 border-[#f58020] border-2 group shadow-md z-50 bg-white hover:shadow-lg  transition-all  xl:col-span-4 col-span-12 rounded-[26px]">
-            
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#212121]  service-card- text-[24px] font-[nexa] mb-4">
-                    App Store Optimization
-                    </h1>
-                    <p className="text-[#212121]  font-[Poppins]">
-                    Enhance app visibility and downloads with strategic optimization in app stores.
-                    </p>
-                  </div>
-                </div>
-             
-            </div>
-
-            <div className="opacity-100 my-4 py-12 p-8 border-[#f58020] border-4 group  shadow-md z-50 bg-white hover:shadow-lg  transition-all  xl:col-span-4 col-span-12 rounded-[26px]">
-             
-             <div className="flex">
-               <div className="">
-                 <h1 className="text-[#f58020]  service-card- text-[34px] font-[nexa] mb-4">
-                 Search Engine Optimization
-                 </h1>
-                 <p className="text-[#212121] font-[Poppins]">
-                 Boost website visibility on search engines and drive organic traffic.
-                 </p>
-               </div>
-             </div>
-      
-         </div>
-
-            <div className="opacity-100 my-4 p-8 border-[#f58020] border-2 group  shadow-md z-50 bg-white hover:shadow-lg  transition-all  xl:col-span-4 col-span-12 rounded-[26px]">
-             
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#212121]  service-card- text-[24px] font-[nexa] mb-4">
-                    Social Media Marketing
-                    </h1>
-                    <p className="text-[#212121]  font-[Poppins]">
-                    Amplify brand presence and engagement through targeted social media strategies
-                    </p>
-                  </div>
-                </div>
-            
-            </div>
-
-           
+    <div className="bg-[#f58020]">
+      <div className=" container mx-auto">
+        <div className="grid py-12 grid-cols-12">
+          <div className="xl:col-span-4 mb-12 xl:mb-0 flex flex-col justify-center items-center col-span-12 p-4 mr-6">
+            <h2 className="relative mt-4 mb-5 md:text-[2.5rem] text-white font-[poppins] font-bold leading-tight">
+              Introduce Your Brand To Digital Media!
+            </h2>
+            <p className=" font-[poppins] text-white text-[26px] leading-9">
+              Your brand will steal the limelight from top tier names in the
+              marketspace with our creative digital services.
+            </p>
           </div>
-          <div className="basis-4/4 md:basis-2/4 px-12">
-            <h1 className="main-banner-heading relative mt-4 mb-12 text-[52px] sm:text-5xl md:text-[5rem] text-[#212121] font-[nexa] font-bold leading-tight text-center xl:text-left">
-              Business
-              <br className='sm:hidden visible' />
-              <span className="mx-4 span-color bg-clip-text text-[#f58020]">
-                Growth
-              </span>
-            </h1>
-            <ul>
-              <li className="flex items-center justify-center xl:justify-start mb-8">
-                <BsFillRocketTakeoffFill className="text-[#f58020] text-[42px] md:text-[52px]" />
-                <p className="leading-normal text-[#212121] opacity-75 ml-6 px-0 md:pr-20 font-sans font-medium text-[22px] md:text-[26px] md:text-left">
-                  Boosting Brands
-                </p>
-              </li>
-              <li className="flex items-center justify-center xl:justify-start mb-7">
-                <IoIosPeople className="text-[#f58020] text-[52px] md:text-[72px]"/>
-                <p className="leading-normal text-[#212121] opacity-75 ml-4 px-0 md:pr-20 font-sans font-medium text-[22px] md:text-[26px] md:text-left">
-                  Targeting Leads
-                </p>
-              </li>
-              <li className="flex items-center justify-center xl:justify-start mb-8">
-              <GiReceiveMoney className="text-[#f58020] text-[52px] md:text-[72px]" />
-                <p className="leading-normal text-[#212121] opacity-75 ml-6 px-0 md:pr-20 font-sans font-medium text-[22px] md:text-[26px] md:text-left">
-                  Maximizing ROI
-                </p>
-              </li>
-            </ul>
-         
-          </div>
-        </div>
-      </div>
 
-<div className="grid grid-cols-12">
-        <div className="xl:col-span-3 mb-12 xl:mb-0 flex flex-col justify-center items-center col-span-12 p-4 bg-white rounded-[26px] mr-6">
-          {/* <img src={services} className="rounded-[26px]" /> */}
-
-          <p className="leading-normal text-[#212121] opacity-75 font-sans font-medium text-[20px] md:text-[22px] mb-8 text-center">
-            boost your online presence and make the most of the digital world!
-          </p>
-        </div>
-
-        <div className="xl:col-span-9 col-span-12">
-          <div className="grid grid-cols-12 gap-4  md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-            <div className="opacity-100 group p-12 px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all hover:bg-[#f58020]   sm:px-7.5 xl:col-span-4 col-span-12 rounded-[26px]">
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#f58020] group-hover:text-white service-card- text-[24px] font-[nexa] mb-4">
-                      Mobile Application
-                    </h1>
-                    <p className="text-[#212121] group-hover:text-white font-[Poppins]">
-                      Seamlessly connect with your audience through our
-                      cutting-edge mobile app solutions
-                    </p>
+          <div className="xl:col-span-8 flex items-center col-span-12">
+            <div className="">
+              <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+                <div className="opacity-100 p-12 flex justify-center items-center group px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all  hover: sm:px-7.5 xl:col-span-3 col-span-12 ">
+                  <div className="flex">
+                    <div className="">
+                      <h1 className="text-[#f58020] text-center service-card- text-[24px] font-[nexa] mb-4">
+                        90%
+                      </h1>
+                      <p className="text-[#212121] font-[Poppins]">
+                        Increase in ROI
+                      </p>
+                    </div>
                   </div>
                 </div>
-            </div>
 
-            <div className="opacity-100 p-12 group px-5 py-7 pb-5 shadow-md z-50 bg-white group-hover:text-white hover:shadow-lg hover:mt-[-12px] transition-all hover:bg-[#f58020]  hover: sm:px-7.5 xl:col-span-4 col-span-12 rounded-[26px]">
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#f58020] group-hover:text-white service-card- text-[24px] font-[nexa] mb-4 ">
-                      Website Development
-                    </h1>
-                    <p className="text-[#212121] group-hover:text-white font-[Poppins]">
-                      The best Website Development solutions for your business
-                      to prosper and reach the global market.{" "}
-                    </p>
+                <div className="opacity-100 p-12 group justify-center items-center px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all  hover: sm:px-7.5 xl:col-span-3 col-span-12 ">
+                  <div className="flex">
+                    <div className="">
+                      <h1 className="text-[#f58020] text-center service-card- text-[24px] font-[nexa] mb-4">
+                        1570+
+                      </h1>
+                      <p className="text-[#212121]  font-[Poppins]">
+                        Project Done{" "}
+                      </p>
+                    </div>
                   </div>
                 </div>
-            </div>
 
-            <div className="opacity-100 p-12 group px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all hover:bg-[#f58020]  hover: sm:px-7.5 xl:col-span-4 col-span-12 rounded-[20px]">
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#f58020] group-hover:text-white service-card- text-[24px] font-[nexa] mb-4">
-                      Social Media Marketing
-                    </h1>
-                    <p className="text-[#212121] group-hover:text-white font-[Poppins]">
-                      Best social media marketing solutions to take your brand
-                      to the next level
-                    </p>
+                <div className="opacity-100 p-12 group justify-center items-center px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all   hover: sm:px-7.5 xl:col-span-3 col-span-12 ">
+                  <div className="flex">
+                    <div className="">
+                      <h1 className="text-[#f58020] text-center service-card- text-[24px] font-[nexa] mb-4">
+                        NFT Design
+                      </h1>
+                      <p className="text-[#212121]  font-[Poppins]">
+                        Daily Website Vists{" "}
+                      </p>
+                    </div>
                   </div>
                 </div>
-            </div>
 
-            <div className="opacity-100 p-12 group px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all hover:bg-[#f58020]  hover: sm:px-7.5 xl:col-span-4 col-span-12 rounded-[26px]">
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#f58020] group-hover:text-white service-card- text-[24px] font-[nexa] mb-4">
-                      2D / 3D Animation
-                    </h1>
-                    <p className="text-[#212121] group-hover:text-white font-[Poppins]">
-                      Bring your imagination to life with our exceptional 2D /
-                      3D animation services
-                    </p>
+                <div className="opacity-100 p-12 group justify-center items-center px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all   hover: sm:px-7.5 xl:col-span-3 col-span-12 ">
+                  <div className="flex">
+                    <div className="">
+                      <h1 className="text-[#f58020] text-center service-card- text-[24px] font-[nexa] mb-4">
+                        NFT Design
+                      </h1>
+                      <p className="text-[#212121]  font-[Poppins]">
+                        Satisfied Customers{" "}
+                      </p>
+                    </div>
                   </div>
                 </div>
-            </div>
-
-            <div className="opacity-100 p-12 group px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all hover:bg-[#f58020]  hover: sm:px-7.5 xl:col-span-4 col-span-12 rounded-[26px]">
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#f58020] group-hover:text-white service-card- text-[24px] font-[nexa] mb-4">
-                      Graphics Design
-                    </h1>
-                    <p className="text-[#212121] group-hover:text-white font-[Poppins]">
-                      We provide custom design solutions that range from Logo
-                      Design to Mobile and Web{" "}
-                    </p>
-                  </div>
-                </div>
-            </div>
-
-            <div className="opacity-100 p-12 group px-5 py-7 pb-5 shadow-md z-50 bg-white hover:shadow-lg hover:mt-[-12px] transition-all hover:bg-[#f58020]  hover: sm:px-7.5 xl:col-span-4 col-span-12 rounded-[26px]">
-                <div className="flex">
-                  <div className="">
-                    <h1 className="text-[#f58020] group-hover:text-white service-card- text-[24px] font-[nexa] mb-4">
-                      NFT Design
-                    </h1>
-                    <p className="text-[#212121] group-hover:text-white font-[Poppins]">
-                      We craft unique NFT designs, elevating your project with
-                      innovative solutions.{" "}
-                    </p>
-                  </div>
-                </div>
-     
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
   );
 };
